fb4d601c74265290a9837d67c70f67d6d8236d4b
kemich <46852267+kemich@users.noreply.github.com>
2019-03-21 13:34:04 +0300

    ...

--- js/script.js
+++ js/script.js
@@ -1,30 +1,24 @@
 (function () {
   "use strict";
 
-  document.querySelector(".js-img-map").style.cssText = "display: none;";
+  document.querySelector(".js-img-map").classList.add("map__img--hidden");
 
   ymaps.ready(init);
 
   var myMap, myPlacemark;
 
   function init() {
-    myMap = new ymaps.Map("ya-map", {
-      center: [59.939523, 30.329727],
+    myMap = new ymaps.Map("map", {
+      center: [34.869497, -111.760186],
       behaviors: ["default", "scrollZoom"],
-      zoom: 16
+      zoom: 8
     });
     myMap.controls
       .add("zoomControl", {
         left: 5,
         top: 5
       })
-    myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
-      balloonContent: "ул. Большая Конюшенная 19/8"
-    }, {
-      iconImageHref: "img/pic-1.jpg",
-      iconImageSize: [80, 140],
-      iconImageOffset: [-40, -140]
-    });
+    myPlacemark = new ymaps.Placemark([34.869497, -111.760186]);
     myMap.geoObjects
       .add(myPlacemark);
   }
