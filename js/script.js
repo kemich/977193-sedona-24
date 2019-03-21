31606b563365f7f42c52c60ae32ca0a58ba61b9d
kemich <46852267+kemich@users.noreply.github.com>
2019-03-21 13:58:34 +0300

    ...

--- js/script.js
+++ js/script.js
@@ -23,3 +23,71 @@
       .add(myPlacemark);
   }
 }());
+
+(function () {
+  "use strict";
+
+  var searchForm, searchFormTrigger, inputs;
+
+  searchForm = document.querySelector(".js-search-form");
+  searchFormTrigger = document.querySelector(".js-search-form-trigger");
+  inputs = searchForm.querySelectorAll("[name]");
+
+  searchFormTrigger.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (searchForm.classList.contains("search-form--show")) {
+      searchForm.classList.remove("search-form--show");
+      inputs[0].focus();
+    } else {
+      searchForm.classList.add("search-form--show");
+    }
+  }, false);
+
+  window.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) {
+      event.preventDefault();
+      if (searchForm.classList.contains("search-form--show")) {
+        searchForm.classList.remove("search-form--show");
+        inputs[0].focus();
+      } else {
+        searchForm.classList.add("search-form--show");
+      }
+    }
+  }, false);
+
+  searchForm.addEventListener("submit", function (event) {
+    var i, input;
+    for (i = inputs.length - 1; i >= 0; i--) {
+      input = inputs[i];
+      if (!input.value) {
+        event.preventDefault();
+        searchForm.classList.remove("search-form--error");
+        searchForm.offsetWidth;
+        searchForm.classList.add("search-form--error");
+        input.focus();
+      }
+    }
+  }, false);
+
+}());
+
+(function () {
+  "use strict";
+  var elements, i, length;
+
+  function setElement(element) {
+    var name = element.getAttribute("name");
+    element.value = localStorage.getItem(name) || "";
+    element.addEventListener("keyup", function (event) {
+      event.preventDefault();
+      localStorage.setItem(name, element.value);
+    }, false);
+  }
+  if (window.localStorage) {
+    elements = document.querySelectorAll("[name]");
+    for (i = 0, length = elements.length; i < length; i = i + 1) {
+      setElement(elements[i]);
+    }
+  }
+
+}());
